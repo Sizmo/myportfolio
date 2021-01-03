fe41a7cff55febff1f54c15518f0bc08644c8914
Simon Ackerberg <simonackerberg@live.co.za>
2021-01-03 14:29:38 +0200

    beta1

--- index.js
+++ index.js
@@ -1,21 +1,42 @@
 const get = (e) => document.getElementById(e)
 
 const door = get('door')
+const doorContainer = get('door-container')
 const beep = get('scanner-beep')
 const temp = get('temperature-reading')
+const tempContainer = get('temp-container')
+const sanitiseBtn = get('sanitise-btn')
+const sanitiseAudio = get('sanitise-audio')
 
 beep.volume = 0.1
 beep.play()
 
-const temperature = () => {
-  let num = Math.random() * 100
-  console.log(num)
+const temperature = (min, max) => {
+  let num = Math.random() * (max - min) + min
   let randomTemp = Math.round(num * 10) / 10
-  console.log(randomTemp)
+  temp.textContent = `${randomTemp}°C`
 }
 
-temperature()
+setTimeout(timer, 2000)
+function timer() {
+  temperature(35, 38)
+}
 
-door.addEventListener('click', (event) => {
+let sanitise = false
+
+sanitiseBtn.addEventListener('click', () => {
+  sanitise = true
+  sanitiseBtn.style.borderColor = 'green'
+  sanitiseBtn.textContent = 'WELCOME!'
+  sanitiseAudio.play()
+  setInterval(doorOpenTimer, 1500)
+})
+
+function doorOpenTimer() {
   door.classList.add('door-animation')
+  tempContainer.style.display = 'none'
+}
+
+door.addEventListener('animationend', () => {
+  doorContainer.style.display = 'none'
 })
